570b0392df2108da0824ef3dc8a0cedb048f0cc1
IvanAlekseenko-sys <ivanalekseenko.post@gmail.com>
2024-10-31 21:03:23 +0100

    Update BubbleSortTest.java

--- lesson_36/code/homework_36/Arrays/BubbleSortTest.java
+++ lesson_36/code/homework_36/Arrays/BubbleSortTest.java
@@ -5,11 +5,11 @@ public class BubbleSortTest {
     public static void main(String[] args) {
 
         int[] newArray = ArrayMethods.fillArray(0, 1000, 10);
-        ArrayMethods.bubbleSort(newArray);
+        ArrayMethods.bubbleSort(newArray); //Количество операций: 64
         newArray = ArrayMethods.fillArray(0, 1000, 100);
-        ArrayMethods.bubbleSort(newArray);
+        ArrayMethods.bubbleSort(newArray); //Количество операций: 7312
         newArray = ArrayMethods.fillArray(0, 1000, 1000);
-        ArrayMethods.bubbleSort(newArray);
+        ArrayMethods.bubbleSort(newArray); //Количество операций: 747325
 
     }
 }
